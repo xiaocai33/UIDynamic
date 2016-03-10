693c57f55cc09f81098333e6001c1d153a88ea7b
xiaocai <tycai1@sina.com>
2016-03-11 00:10:45 +0800

    01新建项目

---
+++ UIDynamic/UIDynamic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIDynamic
+//
+//  Created by 小蔡 on 16/3/11.
+//  Copyright © 2016年 xiaocai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
